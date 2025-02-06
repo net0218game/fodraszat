4ca0b4df6ff688cb0b221eab458af99f51408028
DⱯɅID <game.net0218@gmail.com>
2025-02-06 19:59:02 +0100

    app.js change

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 // Set up session
