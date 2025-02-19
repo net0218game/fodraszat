e95806836899e649ccd5cf666f3e337c43a7e5d8
david <game.net0218@gmail.com>
2025-02-19 23:53:25 +0100

    css fix

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/app', express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
